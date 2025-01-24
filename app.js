7c3ff12d59300350f93fe9bf3b9a1b00554f6084
Mateus Wilcles <matheuswilcles@gmail.com>
2025-01-24 14:35:33 -0400

    Uma função adicionar amigos adicionada em 24/01

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+
+let amigos = [];
+
+function adicionarAmigos() {
+    let input = document.querySelector('input');
+    let nome = input.value.trim();
+
+    if(nome == "") {
+        alert('Ops! Digite um nome.')
+        return;
+    }
+
+    if(amigos.includes(nome)) {
+        alert('Esse nome já esta incluido.');
+        return;
+    }
+
+    amigos.push(nome);
+    input.value = "";
+    ResultadoNomes();
+    
+}
